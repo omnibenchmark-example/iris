47c63f0dfd6194e67bb467d62dc6abff8d54e1db
Daniel Incicau <daniel.incicau@gmail.com>
2024-10-29 18:53:33 +0100

    preprocess dataset before writing it

--- load_iris.py
+++ load_iris.py
@@ -2,7 +2,9 @@ import argparse
 import os
 from pathlib import Path
 
+import pandas as pd
 from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
 
 
 def main():
@@ -18,9 +20,18 @@ def main():
 
     iris = datasets.load_iris(as_frame=True)
 
+    # Prepare features
+    features = iris.data
+    scaler = StandardScaler()
+    features_scaled = scaler.fit_transform(features)
+    features_scaled_df = pd.DataFrame(features_scaled, columns=features.columns)
+
+    # Prepare labels
+    labels_df = iris.target.to_frame(name='label')
+
     # Write to disk
-    iris.data.to_csv(Path(args.output_dir) / f'{args.name}.features.csv', index_label='id')
-    iris.target.to_csv(Path(args.output_dir) / f'{args.name}.labels.csv', index_label='id')
+    features_scaled_df.to_csv(Path(args.output_dir) / f'{args.name}.features.csv', index_label='id')
+    labels_df.to_csv(Path(args.output_dir) / f'{args.name}.labels.csv', index_label='id')
 
 
 if __name__ == "__main__":
